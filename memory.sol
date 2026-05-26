0754190656905a6be672197c9a412eb4962237e9
ChauhanAbhinav2400 <145866171+ChauhanAbhinav2400@users.noreply.github.com>
2026-05-26 22:28:14 +0530

    learning memory in yul

---
+++ memory.sol
@@ -0,0 +1,18 @@
+Suppose we change to:
+
+event Transfer(
+    address indexed from,
+    address indexed to,
+    uint indexed amount
+);
+
+Now all variables are indexed.
+
+Tell me:
+
+Topic0 = ?
+Topic1 = ?
+Topic2 = ?
+Topic3 = ?
+
+Data = ?
