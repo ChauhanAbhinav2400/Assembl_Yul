0ae934985cf75000b3f1aa67135e0d28c8f52c38
ChauhanAbhinav2400 <145866171+ChauhanAbhinav2400@users.noreply.github.com>
2026-06-03 10:45:21 +0530

    practice selector extraction and argument extration exercise

--- basic_operation.sol
+++ basic_operation.sol
@@ -10,7 +10,6 @@ contract BasicOperation {
             } lt(i, 10) {
                 i := add(i, 1)
             } {
-               
                 if iszero(mod(i, 2)) {
                     break
                 }
@@ -22,85 +21,83 @@ contract BasicOperation {
         return i;
     }
 
-    function add(uint256 x , uint256 y ) public pure returns (uint256) {
+    function add(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := add(x,y)
+            result := add(x, y)
         }
         return result;
     }
 
-   function sub(uint256 x , uint256 y ) public pure returns (uint256) {
+    function sub(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := sub(x,y)
+            result := sub(x, y)
         }
         return result;
     }
 
-    function mul(uint256 x , uint256 y ) public pure returns (uint256) {
+    function mul(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := mul(x,y)
+            result := mul(x, y)
         }
         return result;
     }
 
-    function div(uint256 x , uint256 y ) public pure returns (uint256) {
+    function div(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := div(x,y)
+            result := div(x, y)
         }
         return result;
     }
 
-    function and(uint256 x , uint256 y ) public pure returns (uint256) {
+    function and(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := and(x,y)
+            result := and(x, y)
         }
         return result;
     }
 
-    function or(uint256 x , uint256 y ) public pure returns (uint256) {
+    function or(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := or(x,y)
+            result := or(x, y)
         }
         return result;
     }
 
-
-    function xor(uint256 x , uint256 y ) public pure returns (uint256) {
+    function xor(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := xor(x,y)
+            result := xor(x, y)
         }
         return result;
     }
-    
 
-    function lessthan(uint256 x , uint256 y ) public pure returns (bool) {
+    function lessthan(uint256 x, uint256 y) public pure returns (bool) {
         bool result;
         assembly {
-            result := lt(x,y)
+            result := lt(x, y)
         }
         return result;
     }
 
-    function greaterthan(uint256 x , uint256 y ) public pure returns (bool) {
+    function greaterthan(uint256 x, uint256 y) public pure returns (bool) {
         bool result;
-        assembly{
-            result := gt(x,y)
+        assembly {
+            result := gt(x, y)
         }
         return result;
     }
 
-
-    function modulo(uint256 x , uint256 y ) public pure returns (uint256) {
+    function modulo(uint256 x, uint256 y) public pure returns (uint256) {
         uint256 result;
         assembly {
-            result := mod(x,y)
+            result := maq
+            od(x, y)
         }
         return result;
     }
--- inter-contract_calls.sol
+++ inter-contract_calls.sol
@@ -2,216 +2,181 @@
 pragma solidity ^0.8.20;
 
 contract CalldataBootcamp1 {
-
-    function ex1()
-        external
-        pure
-        returns(uint256 size)
-    {
-       // load size of calldata
-       assembly {
-        size := calldatasize()
-       }
+    function ex1() external pure returns (uint256 size) {
+        // load size of calldata
+        assembly {
+            size := calldatasize()
+        }
     }
 
-    function ex2()
-    external
-    pure
-    returns(bytes32 value)
-{
-   // load selector 
-   assembly {
-    value := calldataload(0)
-   }
-}
- 
- function ex3(
-    uint256 x
-)
-    external
-    pure
-    returns(bytes32 value)
-{
-    // load x 
-    assembly {
-        value := calldataload(4)
+    function ex2() external pure returns (bytes32 value) {
+        // load selector
+        assembly {
+            value := calldataload(0)
+        }
+    }
+
+    function ex3(uint256 x) external pure returns (bytes32 value) {
+        // load x
+        assembly {
+            value := calldataload(4)
+        }
+    }
+
+    function ex4(
+        uint256 x,
+        uint256 y
+    ) external pure returns (bytes32 a, bytes32 b) {
+        assembly {
+            a := calldataload(4)
+            b := calldataload(36)
+        }
+    }
+
+    function ex5() external pure returns (bytes32 value) {
+        assembly {
+            value := calldataload(999)
+        }
     }
 }
-
-
-function ex4(
-    uint256 x,
-    uint256 y
-)
-    external
-    pure
-    returns(
-        bytes32 a,
-        bytes32 b
-    )
-{
-    assembly {
-        a := calldataload(4)
-        b := calldataload(36)
-
-    }
-}
-
-
-function ex5()
-    external
-    pure
-    returns(bytes32 value)
-{
-   assembly {
-    value := calldataload(999)
-   }
-}
-
-}
-
-
 
 //=============================================================================================
 
-
-
-contract CalldataBootcamp2 {  
-
-
-function ex1()
-    external
-    pure
-    returns(bytes32 value)
-{
-    assembly {
-   calldatacopy(
-    0x80 ,
-    0,
-    calldatasize()
-   )
-   value := mload(0x80)
+contract CalldataBootcamp2 {
+    function ex1() external pure returns (bytes32 value) {
+        assembly {
+            calldatacopy(0x80, 0, calldatasize())
+            value := mload(0x80)
+        }
     }
-   
-}
 
+    function ex2() external pure returns (bytes32 value) {
+        assembly {
+            calldatacopy(0x80, 0, 4)
 
-function ex2()
-    external
-    pure
-    returns(bytes32 value)
-{
-    assembly {
-
-        calldatacopy(
-            0x80,
-            0,
-            4
-        )
-
-        value := mload(0x80)
-
+            value := mload(0x80)
+        }
     }
-}
 
+    function ex3(uint256 x) external pure returns (bytes32 value) {
+        assembly {
+            calldatacopy(0x80, 4, 32)
 
-function ex3(
-    uint256 x
-)
-    external
-    pure
-    returns(bytes32 value)
-{
-    assembly {
+            value := mload(0x80)
+        }
+    }
 
-        calldatacopy(
-            0x80,
-            4,
-            32
-        )
+    function ex4(
+        uint256 a,
+        uint256 b
+    ) external pure returns (bytes32 first, bytes32 second) {
+        assembly {
+            calldatacopy(0x80, 4, 64)
 
-        value := mload(0x80)
+            first := mload(0x80)
 
+            second := mload(add(0x80, 32))
+        }
+    }
+
+    function ex5(uint256 x) external pure returns (bytes32 value) {
+        assembly {
+            calldatacopy(0x80, 20, 8)
+
+            value := mload(0x80)
+        }
+    }
+
+    function ex6() external pure returns (bytes32 value) {
+        assembly {
+            calldatacopy(0x80, 999, 32)
+
+            value := mload(0x80)
+        }
+    }
+
+    function getselectorofex1() public pure returns (bytes4 selector) {
+        return bytes4(keccak256("ex1()"));
     }
 }
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SelectorBootcamp {
+    function ex1() external pure returns (bytes4 selector) {
+        //return selector with right shift
+        assembly {
+            selector := shr(224, calldataload(0))
+        }
+    }
 
-function ex4(
-    uint256 a,
-    uint256 b
-)
-    external
-    pure
-    returns(
-        bytes32 first,
-        bytes32 second
-    )
-{
-    assembly {
+    function ex2() external pure returns (uint256 result) {
+        assembly {
+            let selector := shr(224, calldataload(0))
 
-        calldatacopy(
-            0x80,
-            4,
-            64
-        )
+            /*
+            Replace this
+            with your own
+            selector
+        */
 
-        first := mload(0x80)
+            if eq(selector, 0x12345678) {
+                result := 111
+            }
+        }
+    }
 
-        second := mload(
-            add(
-                0x80,
-                32
-            )
-        )
+    function ex3() external pure {
+        assembly {
+            // selector match with switch statement
+            let selector := calldataload(0)
+            switch selector
+            case 0x12345678 {
+                mstore(0, 2)
+                return(0, 32)
+            }
+            case 0xabcdef12 {
+                mstore(0, 3)
+                return(0, 32)
+            }
+            default {
+                mstore(0, 4)
+                return(0, 32)
+            }
+        }
+    }
+
+    function ex4() external pure returns (uint256 x) {
+        assembly {
+            x := calldataload(4)
+        }
+    }
+
+    function ex5() external pure returns (uint256 x) {
+        assembly {
+            // if calldata size is less than 36 revert otherwise return x - selctor
+            let size := calldatasize()
+            if lt(size, 36) {
+                revert(0, 0)
+            }
+            x := calldataload(4)
+        }
+    }
+
+    function ex6(uint256 arg) external pure returns (uint256) {
+        assembly {
+            // use helper function and if arg = 8 return 88 otherwose return 99 and store in memory and return it
+            let result := helper(arg)
+            mstore(0x80, result)
+            return(0x80, 32)
+            function helper(x) -> r {
+                if eq(x, 8) {
+                    r := 88
+                    leave
+                }
+                r := 99
+            }
+        }
     }
 }
-
-
-
-function ex5(
-    uint256 x
-)
-    external
-    pure
-    returns(bytes32 value)
-{
-    assembly {
-
-        calldatacopy(
-            0x80,
-            20,
-            8
-        )
-
-        value := mload(0x80)
-
-    }
-}
-
-
-
-function ex6()
-    external
-    pure
-    returns(bytes32 value)
-{
-    assembly {
-
-        calldatacopy(
-            0x80,
-            999,
-            32
-        )
-
-        value := mload(0x80)
-
-    }
-}
-
-function getselectorofex1 () pure public returns(bytes4 selector) {
-    return bytes4(keccak256("ex1()"));
-}
-
-
-} 
